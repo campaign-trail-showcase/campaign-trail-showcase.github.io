e72d4a81f252015bc0e66a2b8130220226e42e6d
Jet Simon <jetjdsimon@gmail.com>
2023-07-19 21:46:44 -0700

    First dummy version of grid

--- static/js/modLoaderCode.js
+++ static/js/modLoaderCode.js
@@ -1,77 +1,133 @@
 
-$(document).ready(function() {
+$(document).ready(async function() {
     var originalOptions = null;
 
     $('.tagCheckbox').on('change', filterEntries);
 
-    loadEntries();
+    await loadEntries();
+    let mods = document.getElementById('modSelect').childNodes;
 
-    function loadEntries() {
-        $.ajax({
-            type: "GET",
-            url: "../static/mods/MODLOADERFILE.html",
-            dataType: "text",
-            success: function(response) {
-                $("#modSelect").html(response);
+    mods.forEach(async function(mod) {
 
-                // hot load
-                let hotload = window.localStorage.getItem("hotload");
-                if (hotload) {
-                    try {
-                        $("#modSelect")[0].value = hotload;
-                        campaignTrail_temp.hotload = hotload;
-                        $("#submitMod").click();
-                    } catch {
+        if(mod.value == "other") {
+            return;
+        }
 
-                    }
-                    window.localStorage.removeItem("hotload") // this should be done whether or not there is an error.
-                }
+        const modRes = await fetch("../static/mods/" + mod.value + "_init.html");
+        const rawModText = await modRes.text();
+        let codeSnippet = null;
+        let temp = {}
+        let start = ""
+        let end = ""
 
-                //clone so we don't reduce the list of mods every time a tag is selected
-                originalOptions = $("#modSelect option").clone();
-                filterEntries();
-            },
-            error: function() {
-                console.log("Error loading mod loader - couldn't reach server.");
-            }
-        });
-    }
+        if(rawModText.includes(".election_json = JSON.parse(")) {
+            start = ".election_json = JSON.parse(";
+            end = ")"
+        } else if(rawModText.includes(".election_json = [")) {
+            start = ".election_json = [";
+            end = "]"
+        } else {
+            console.log("Could not extract metadata for mod: " + mod.value);
+        }
 
-    function filterEntries() {
-        var selectedTags = [];
+        let possibleEndIndices = getAllIndexes(rawModText, end);
 
-        // Get all selected tags
-        $('.tagCheckbox:checked').each(function() {
-            selectedTags.push($(this).val());
-        });
-
-        var filteredOptions = originalOptions.filter(function() {
-            var entryTags = $(this).data('tags');
-
-            if (selectedTags.length === 0) {
-                // Show all if no tags are selected
-                return true;
+        for(let i = 0; i < possibleEndIndices.length; i++) {
+            codeSnippet = rawModText.slice(rawModText.indexOf(start), possibleEndIndices[i] + 1);
+            if(codeSnippet.length <= 0) {
+                continue;
             }
 
-            //return mods that are tagged and have all checked tags
-            return entryTags && (containsAllTags(entryTags, selectedTags));
-        });
+            try {
+                eval("temp" + codeSnippet)
+            } catch {
+                codeSnippet = null;
+            }
 
-        var $modSelect = $('#modSelect');
-        $modSelect.empty().append(filteredOptions);
-
-        $modSelect.val($modSelect.find('option:first').val());
-    }
-
-    function containsAllTags(entryTags, selectedTags) {
-        var entryTagArray = entryTags.split(' ');
-
-        for (var i = 0; i < selectedTags.length; i++) {
-            if (!entryTagArray.includes(selectedTags[i])) {
-                return false;
+            if(codeSnippet != null) {
+                break;
             }
         }
 
-        return true;
-    }
+        if(codeSnippet == null || Object.keys(temp).length == 0) {
+            console.log("Could not extract from " + mod.value)
+        }
+
+        let imageUrl = temp.election_json[0].fields.image_url;
+        let description = temp.election_json[0].fields.summary.slice(0, 300) + "...";
+       
+        document.getElementById("mod-grid").appendChild(createModView(mod, imageUrl, description));
+    });
+    
 });
+
+function createModView(mod, imageUrl, description) {
+    const modView = document.createElement("div");
+    modView.classList.add("community-grid-element")
+
+    modView.innerHTML = `
+    <h1>${mod.value}</h1>
+    <img style="width:80%; border: 15px solid white;" src="${imageUrl}"></img>
+    <div class="mod-desc">${description}</div>
+    <button>Load Mod</button>
+    <p>LOAD MOD BUTTON NOT WORKING YET</p>
+    `
+
+    return modView;
+}
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+    return indexes;
+}
+
+async function loadEntries() {
+    
+    const modList = await fetch("../static/mods/MODLOADERFILE.html");
+
+    $("#modSelect").html(await modList.text());
+    //clone so we don't reduce the list of mods every time a tag is selected
+    originalOptions = $("#modSelect option").clone();
+    filterEntries();
+}
+
+function filterEntries() {
+    var selectedTags = [];
+
+    // Get all selected tags
+    $('.tagCheckbox:checked').each(function() {
+        selectedTags.push($(this).val());
+    });
+
+    var filteredOptions = originalOptions.filter(function() {
+        var entryTags = $(this).data('tags');
+
+        if (selectedTags.length === 0) {
+            // Show all if no tags are selected
+            return true;
+        }
+
+        //return mods that are tagged and have all checked tags
+        return entryTags && (containsAllTags(entryTags, selectedTags));
+    });
+
+    var $modSelect = $('#modSelect');
+    $modSelect.empty().append(filteredOptions);
+
+    $modSelect.val($modSelect.find('option:first').val());
+}
+
+function containsAllTags(entryTags, selectedTags) {
+    var entryTagArray = entryTags.split(' ');
+
+    for (var i = 0; i < selectedTags.length; i++) {
+        if (!entryTagArray.includes(selectedTags[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
