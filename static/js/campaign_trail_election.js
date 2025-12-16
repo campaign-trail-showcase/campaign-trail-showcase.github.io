283c5dffee3525f14c43c4c8103534f978f49b06
marc12267 <marc1my2words2@gmail.com>
2025-12-16 08:39:06 -0800

    whoops

--- static/js/campaign_trail_election.js
+++ static/js/campaign_trail_election.js
@@ -135,7 +135,7 @@ function csrfToken() {
     if (document.cookie && document.cookie != "") {
       var cookies = document.cookie.split(";");
       for (var i = 0; i < cookies.length; i++) {
-        var cookie = i[a].trim(cookies[i]);
+        var cookie = cookies[i].trim();
         // Does this cookie string begin with the name we want?
         if (cookie.substring(0, name.length + 1) == name + "=") {
           cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
